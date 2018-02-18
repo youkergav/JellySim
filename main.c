a9fb3ed932d47bde43e3d2804d09e3ac9abd0130
Gavin Youker <youkergav@gmail.com>
2018-02-18 13:39:53 -0500

    graphics overhaul

--- main.c
+++ main.c
@@ -27,24 +27,27 @@ int main() {
     }
 
     // RIP </3
-    printf("BLEEP!");
+    printf("\n\n       ...BLEEP!\n\n");
 }
 
 // Function that defines everything of a jellyfish.
 void swim(motion) {
-    // Change animation per frame.
-    if(motion % 2 == 0) {
-        printf(" ___\n(   )\n ---\n/ | \\");
-    } else {
-        printf(" ___\n(   )\n ---\n\\ | /");
-    }
+    // Print half of jellyfish.
+    printf(" ___\n(   )\n ---");
 
     // Probability of blimps and bloomps.
     int stuff = rand() % 2;
     if(stuff == 0) {
-        printf("  ...Blimp.");
+        printf("   ...Blimp.\n");
     } else {
-        printf("  ...Bloop.");
+        printf("   ...Bloop.\n");
+    }
+
+    // Animate jellyfish legs.
+    if(motion % 2 == 0) {
+        printf("/ | \\\n");
+    } else {
+        printf(" \\|/\n");
     }
 
     // Advanced delay algorithm.
